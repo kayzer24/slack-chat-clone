5be8f99c5a04894f6c994571c1d5e2a97e54571f
kayzer24 <dante2410@hotmail.fr>
2021-01-24 00:44:58 +0100

    typing component_animation, display in messages

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -7,6 +7,7 @@ import {Segment, Comment} from 'semantic-ui-react';
 import MessagesHeader from './MessagesHeader';
 import MessageForm from './MessageForm';
 import Message from './Message';
+import Typing from './Typing';
 
 class Messages extends React.Component {
 	state = {
@@ -196,6 +197,12 @@ class Messages extends React.Component {
 				<Segment>
 					<Comment.Group className={progressBar ? 'messages_progress' : 'messages'}>
 						{searchTerm ? this.displayMessages(searchResults) : this.displayMessages(messages)}
+						
+						<div style={{ display: "flex" , alignItems: "center" }}>
+							<span className="user__typing">
+								kayzer is typing
+							</span> <Typing />
+						</div>
 					</Comment.Group>
 				</Segment>
 
